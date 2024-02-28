b4e8636ffaf6280f19fa4e9d26dbf907ca3e1ac2
Nursultan <nursultanyelemessov@gmail.com>
2024-02-28 20:06:28 +0600

    Initial Commit

---
+++ TinyWeather/TinyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyWeatherApp.swift
+//  TinyWeather
+//
+//  Created by Nursultan Yelemessov on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
